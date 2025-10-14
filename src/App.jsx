6c8780ca7812edb961ed36061b6779a365493b44
markelstringfellow <markelstringfellow@gmail.com>
2025-10-13 22:46:43 -0400

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Rawtopian Bliss</h1>
+      <p>Your React app is now live!</p>
+    </div>
+  );
+}
